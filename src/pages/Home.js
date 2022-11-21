3a9c30811281183707f5ca4218c7486594b49a6d
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-21 13:54:29 -0500

    add post form working with state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  const [posts, setPosts] = useState([])
+  
+  useEffect(() => {
+    fetch("http://localhost:4040/")
+      .then(res => res.json())
+      .then(data => setPosts(data))
+      .catch(err => console.error(err))
+  },[])
+
+  // console.log(posts)
+
+  const AllPosts = () => {
+    return (
+      <div className="posts">
+        {posts.map((item, index) => {
+          return(
+            <div className="post-item" key={item._id}>
+              <img src={`https://source.unsplash.com/random?sig=${index}`} alt="" />
+              <h4>{item.author}</h4>
+              <p>{item.text}</p>
+              <p>{item.date}</p>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
   return (
-    <h1>Home</h1>
+    <div className="container">
+      <h1>Home Component</h1>
+      <AllPosts />
+    </div>
   )
 }
